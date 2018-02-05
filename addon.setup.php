e8e0f1d31b94fdd3ba41ba63b251ead1ef25010a
Tim Murtaugh <tim@o2b.net>
2018-02-05 16:02:48 -0500

    Initial commit

---
+++ addon.setup.php
@@ -0,0 +1,16 @@
+<?php
+
+return array(
+	'name' => 'Strong Passord Generator',
+	'version' =>'1.0.0',
+	'author' =>'Tim Murtaugh',
+	'author_url' => 'http://monkeydo.biz',
+	'description' => 'Returns a strong password.',
+	'namespace' => 'Stronggen\Stronggen',
+	'plugin.usage' => array(
+		'description' => 'Returns a strong password.',
+		'example' => '{exp:strongpass}'
+	)
+);
+/* End of file addon.setup.php */
+/* Location: /system/user/addons/stronggen/addon.setup.php */
